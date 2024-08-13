7ed3b8e2b642d15d628cec3c377c6cb2b4eaebd7
TheRootED24 <TheRootEd24@gmail.com>
2024-08-12 22:12:56 -0400

    Update mgjson.h
    Update src to latest Mongoose Version 7.14

--- src/mgjson.h
+++ src/mgjson.h
@@ -1,5 +1,5 @@
 // Copyright (c) 2004-2013 Sergey Lyubka
-// Copyright (c) 2013-2022 Cesanta Software Limited
+// Copyright (c) 2013-2024 Cesanta Software Limited
 // All rights reserved
 //
 // This software is dual-licensed: you can redistribute it and/or modify
@@ -20,55 +20,18 @@
 #ifndef MGJSON_H
 #define MGJSON_H
 
-#define MG_VERSION "7.10"
+#define MG_VERSION "7.14"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define MG_ARCH_UNIX 1         // Linux, BSD, Mac, ...
-
-#if !defined(MG_ARCH)
-#if defined(__unix__) || defined(__APPLE__)
-#define MG_ARCH MG_ARCH_UNIX
-#endif  // !defined(MG_ARCH)
-
-// if the user did not specify an MG_ARCH, or specified a custom one, OR
-// we guessed a known IDE, pull the customized config (Configuration Wizard)
-#if !defined(MG_ARCH) || (MG_ARCH == MG_ARCH_CUSTOM) || MG_ARCH == MG_ARCH_ARMCC
-#include "mongoose_custom.h"  // keep this include
-#endif
-
-#if !defined(MG_ARCH)
-#error "MG_ARCH is not specified and we couldn't guess it. Set -D MG_ARCH=..."
-#endif
-
-// http://esr.ibiblio.org/?p=5095
-#define MG_BIG_ENDIAN (*(uint16_t *) "\0\xff" < 0x100)
-
-#if MG_ARCH == MG_ARCH_UNIX
-#define _DARWIN_UNLIMITED_SELECT 1  // No limit on file descriptors
-
-#if defined(__APPLE__)
-#include <mach/mach_time.h>
-#endif
-
-#if !defined(MG_ENABLE_EPOLL) && defined(__linux__)
-#define MG_ENABLE_EPOLL 1
-#elif !defined(MG_ENABLE_POLL)
-#define MG_ENABLE_POLL 1
-#endif
-
-#include <arpa/inet.h>
 #include <ctype.h>
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <inttypes.h>
 #include <limits.h>
-#include <netdb.h>
-#include <netinet/in.h>
-#include <netinet/tcp.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdbool.h>
@@ -78,15 +41,6 @@ extern "C" {
 #include <stdlib.h>
 #include <string.h>
 
-#if defined(MG_ENABLE_EPOLL) && MG_ENABLE_EPOLL
-#include <sys/epoll.h>
-#elif defined(MG_ENABLE_POLL) && MG_ENABLE_POLL
-#include <poll.h>
-#else
-#include <sys/select.h>
-#endif
-
-#include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -97,17 +51,9 @@ extern "C" {
 #define MG_ENABLE_DIRLIST 1
 #endif
 
-#ifndef MG_PATH_MAX
-#define MG_PATH_MAX FILENAME_MAX
-#endif
-
-
-
 #ifndef MG_ENABLE_CUSTOM_MILLIS
 #define MG_ENABLE_CUSTOM_MILLIS 0
 #endif
-
-
 
 #ifndef MG_ENABLE_ASSERT
 #define MG_ENABLE_ASSERT 0
@@ -117,44 +63,25 @@ extern "C" {
 #define MG_IO_SIZE 2048  // Granularity of the send/recv IO buffer growth
 #endif
 
-
+// Describes an arbitrary chunk of memory
 struct mg_str {
-  const char *ptr;  // Pointer to string data
-  size_t len;       // String len
+  char *buf;   // String data
+  size_t len;  // String length
 };
-
-#define MG_NULL_STR \
-  { NULL, 0 }
-
-#define MG_C_STR(a) \
-  { (a), sizeof(a) - 1 }
 
 // Using macro to avoid shadowing C++ struct constructor, see #1298
 #define mg_str(s) mg_str_s(s)
 
 struct mg_str mg_str(const char *s);
 struct mg_str mg_str_n(const char *s, size_t n);
-int mg_lower(const char *s);
-int mg_ncasecmp(const char *s1, const char *s2, size_t len);
 int mg_casecmp(const char *s1, const char *s2);
-int mg_vcmp(const struct mg_str *s1, const char *s2);
-int mg_vcasecmp(const struct mg_str *str1, const char *str2);
 int mg_strcmp(const struct mg_str str1, const struct mg_str str2);
-struct mg_str mg_strstrip(struct mg_str s);
+int mg_strcasecmp(const struct mg_str str1, const struct mg_str str2);
 struct mg_str mg_strdup(const struct mg_str s);
-const char *mg_strstr(const struct mg_str haystack, const struct mg_str needle);
 bool mg_match(struct mg_str str, struct mg_str pattern, struct mg_str *caps);
-bool mg_globmatch(const char *pattern, size_t plen, const char *s, size_t n);
-bool mg_commalist(struct mg_str *s, struct mg_str *k, struct mg_str *v);
-bool mg_split(struct mg_str *s, struct mg_str *k, struct mg_str *v, char delim);
-char *mg_hex(const void *buf, size_t len, char *dst);
-void mg_unhex(const char *buf, size_t len, unsigned char *to);
-unsigned long mg_unhexn(const char *s, size_t len);
-int mg_check_ip_acl(struct mg_str acl, uint32_t remote_ip);
-char *mg_remove_double_dots(char *s);
+bool mg_span(struct mg_str s, struct mg_str *a, struct mg_str *b, char delim);
 
-
-
+bool mg_str_to_num(struct mg_str, int base, void *val, size_t val_len);
 
 // Single producer, single consumer non-blocking queue
 
@@ -170,9 +97,6 @@ size_t mg_queue_book(struct mg_queue *, char **buf, size_t);  // Reserve space
 void mg_queue_add(struct mg_queue *, size_t);                 // Add new message
 size_t mg_queue_next(struct mg_queue *, char **);  // Get oldest message
 void mg_queue_del(struct mg_queue *, size_t);      // Delete oldest message
-
-
-
 
 typedef void (*mg_pfn_t)(char, void *);                  // Output function
 typedef size_t (*mg_pm_t)(mg_pfn_t, void *, va_list *);  // %M printer
@@ -192,11 +116,6 @@ size_t mg_queue_printf(struct mg_queue *, const char *fmt, ...);
 size_t mg_print_base64(void (*out)(char, void *), void *arg, va_list *ap);
 size_t mg_print_esc(void (*out)(char, void *), void *arg, va_list *ap);
 size_t mg_print_hex(void (*out)(char, void *), void *arg, va_list *ap);
-size_t mg_print_ip(void (*out)(char, void *), void *arg, va_list *ap);
-size_t mg_print_ip_port(void (*out)(char, void *), void *arg, va_list *ap);
-size_t mg_print_ip4(void (*out)(char, void *), void *arg, va_list *ap);
-size_t mg_print_ip6(void (*out)(char, void *), void *arg, va_list *ap);
-size_t mg_print_mac(void (*out)(char, void *), void *arg, va_list *ap);
 
 // Various output functions
 void mg_pfn_iobuf(char ch, void *param);  // param: struct mg_iobuf *
@@ -205,39 +124,23 @@ void mg_pfn_stdout(char c, void *param);  // param: ignored
 // A helper macro for printing JSON: mg_snprintf(buf, len, "%m", MG_ESC("hi"))
 #define MG_ESC(str) mg_print_esc, 0, (str)
 
-
-
-
-
-
-
-
-
-
-#if MG_ENABLE_ASSERT
-#include <assert.h>
-#elif !defined(assert)
-#define assert(x)
-#endif
-
+void mg_bzero(volatile unsigned char *buf, size_t len);
 void mg_random(void *buf, size_t len);
 char *mg_random_str(char *buf, size_t len);
-uint16_t mg_ntohs(uint16_t net);
-uint32_t mg_ntohl(uint32_t net);
-uint32_t mg_crc32(uint32_t crc, const char *buf, size_t len);
-uint64_t mg_millis(void);
 
-#define mg_htons(x) mg_ntohs(x)
-#define mg_htonl(x) mg_ntohl(x)
 
-#define MG_U32(a, b, c, d)                                         \
-  (((uint32_t) ((a) &255) << 24) | ((uint32_t) ((b) &255) << 16) | \
-   ((uint32_t) ((c) &255) << 8) | (uint32_t) ((d) &255))
+#define MG_U32(a, b, c, d)                                           \
+  (((uint32_t) ((a) & 255) << 24) | ((uint32_t) ((b) & 255) << 16) | \
+   ((uint32_t) ((c) & 255) << 8) | (uint32_t) ((d) & 255))
 
-// For printing IPv4 addresses: printf("%d.%d.%d.%d\n", MG_IPADDR_PARTS(&ip))
-#define MG_U8P(ADDR) ((uint8_t *) (ADDR))
-#define MG_IPADDR_PARTS(ADDR) \
-  MG_U8P(ADDR)[0], MG_U8P(ADDR)[1], MG_U8P(ADDR)[2], MG_U8P(ADDR)[3]
+
+#define MG_REG(x) ((volatile uint32_t *) (x))[0]
+#define MG_BIT(x) (((uint32_t) 1U) << (x))
+#define MG_SET_BITS(R, CLRMASK, SETMASK) (R) = ((R) & ~(CLRMASK)) | (SETMASK)
+
+#define MG_ROUND_UP(x, a) ((a) == 0 ? (x) : ((((x) + (a) -1) / (a)) * (a)))
+#define MG_ROUND_DOWN(x, a) ((a) == 0 ? (x) : (((x) / (a)) * (a)))
+
 
 // Linked list management macros
 #define LIST_ADD_HEAD(type_, head_, elem_) \
@@ -261,17 +164,6 @@ uint64_t mg_millis(void);
   } while (0)
 
 
-
-unsigned short mg_url_port(const char *url);
-int mg_url_is_ssl(const char *url);
-struct mg_str mg_url_host(const char *url);
-struct mg_str mg_url_user(const char *url);
-struct mg_str mg_url_pass(const char *url);
-const char *mg_url_uri(const char *url);
-
-
-
-
 struct mg_iobuf {
   unsigned char *buf;  // Pointer to stored data
   size_t size;         // Total size available
@@ -285,36 +177,10 @@ void mg_iobuf_free(struct mg_iobuf *);
 size_t mg_iobuf_add(struct mg_iobuf *, size_t, const void *, size_t);
 size_t mg_iobuf_del(struct mg_iobuf *, size_t ofs, size_t len);
 
-int mg_base64_update(unsigned char p, char *to, int len);
-int mg_base64_final(char *to, int len);
-int mg_base64_encode(const unsigned char *p, int n, char *to);
-int mg_base64_decode(const char *src, int n, char *dst);
-
-
-
-
-typedef struct {
-  uint32_t buf[4];
-  uint32_t bits[2];
-  unsigned char in[64];
-} mg_md5_ctx;
-
-void mg_md5_init(mg_md5_ctx *c);
-void mg_md5_update(mg_md5_ctx *c, const unsigned char *data, size_t len);
-void mg_md5_final(mg_md5_ctx *c, unsigned char[16]);
-
-
-
-
-typedef struct {
-  uint32_t state[5];
-  uint32_t count[2];
-  unsigned char buffer[64];
-} mg_sha1_ctx;
-
-void mg_sha1_init(mg_sha1_ctx *);
-void mg_sha1_update(mg_sha1_ctx *, const unsigned char *data, size_t len);
-void mg_sha1_final(unsigned char digest[20], mg_sha1_ctx *);
+size_t mg_base64_update(unsigned char input_byte, char *buf, size_t len);
+size_t mg_base64_final(char *buf, size_t len);
+size_t mg_base64_encode(const unsigned char *p, size_t n, char *buf, size_t);
+size_t mg_base64_decode(const char *src, size_t n, char *dst, size_t);
 
 
 #ifndef MG_JSON_MAX_DEPTH
@@ -325,6 +191,7 @@ void mg_sha1_final(unsigned char digest[20], mg_sha1_ctx *);
 enum { MG_JSON_TOO_DEEP = -1, MG_JSON_INVALID = -2, MG_JSON_NOT_FOUND = -3 };
 int mg_json_get(struct mg_str json, const char *path, int *toklen);
 
+struct mg_str mg_json_get_tok(struct mg_str json, const char *path);
 bool mg_json_get_num(struct mg_str json, const char *path, double *v);
 bool mg_json_get_bool(struct mg_str json, const char *path, bool *v);
 long mg_json_get_long(struct mg_str json, const char *path, long dflt);
@@ -336,10 +203,7 @@ bool mg_json_unescape(struct mg_str str, char *buf, size_t len);
 size_t mg_json_next(struct mg_str obj, size_t ofs, struct mg_str *key,
                     struct mg_str *val);
 
-
-#endif
-#endif
 #ifdef __cplusplus
 }
-#endif
 #endif  // MONGOOSE_H
+#endif
